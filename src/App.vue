a5eef035e5e8bad88312961465004d320969e18a
yu <1134923982@qq.com>
2019-07-20 20:49:27 +0800

    [feat: add todo item method and display in todo list]

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,55 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <div>
-        <h2>Jquery To Do List</h2>
-        <p><em>Simple Todo List with adding and filter by diff status.</em></p>
-      </div>
-      <div>
-        <input class="input-text" type="text" name="ListItem">
-        <div id="button">Add</div>
-      </div>
-      <br>
-      <ol></ol>
-      <div>
-        <div id="filters">
-          <span><a>All</a></span>
-          <span><a>Active</a></span>
-          <span><a>Complete</a></span>
-        </div>
-      </div>
+    <div id="app">
+        <div class="container">
+            <div>
+                <h2>Jquery To Do List</h2>
+                <p><em>Simple Todo List with adding and filter by diff status.</em></p>
+            </div>
+            <div>
+                <input class="input-text" v-model="item.content" type="text" name="ListItem">
+                <div id="button" @click="add">Add</div>
+            </div>
+            <br>
+            <ol>
+                <li v-for="item in itemList">
+                    <span>{{item.content}}</span>
+                </li>
+            </ol>
+            <div>
+                <div id="filters">
+                    <span><a>All</a></span>
+                    <span><a>Active</a></span>
+                    <span><a>Complete</a></span>
+                </div>
+            </div>
 
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 
-export default {
-  name: 'app',
-  components: {
-  }
-}
+    export default {
+        name: 'app',
+        data: function () {
+            return {
+                item: {},
+                itemList: []
+            }
+        },
+        methods: {
+            add: function () {
+                if (this.item.content) {
+                    this.item.status = false;
+                    this.itemList.push(this.item);
+                    this.item = {};
+                }
+            }
+        }
+    }
 </script>
 
 <style>
-  @import "assets/css/style.css";
+    @import "assets/css/style.css";
 
 </style>
