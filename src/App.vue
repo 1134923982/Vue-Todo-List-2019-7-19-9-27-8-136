443641772214fc8eae67b3953432a880d3f230c4
1134923982 <1134923982@qq.com>
2019-07-20 15:55:53 +0800

    [init : project]

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <CounterGroup/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+  @import "assets/css/style.css";
+
+</style>
