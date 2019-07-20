ab5a0927ffdfa4402d53c16d75203240c58c45c3
1134923982 <1134923982@qq.com>
2019-07-20 16:37:09 +0800

    [feat: design list page and update style.css for Todo List]

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,20 @@
         <h2>Jquery To Do List</h2>
         <p><em>Simple Todo List with adding and filter by diff status.</em></p>
       </div>
+      <div>
+        <input class="input-text" type="text" name="ListItem">
+        <div id="button">Add</div>
+      </div>
+      <br>
+      <ol></ol>
+      <div>
+        <div id="filters">
+          <span><a>All</a></span>
+          <span><a>Active</a></span>
+          <span><a>Complete</a></span>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
