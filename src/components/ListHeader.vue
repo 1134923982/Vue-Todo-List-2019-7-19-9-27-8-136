a85ef6f3c318415f5e5c89e3dce22c99590fa623
1134923982 <1134923982@qq.com>
2019-07-22 19:23:04 +0800

    [feat: create ListHeader.vue component for todoList]

---
+++ src/components/ListHeader.vue
@@ -0,0 +1,38 @@
+<template>
+        <div>
+            <div>
+                <h2>Jquery To Do List</h2>
+                <p><em>Simple Todo List with adding and filter by diff status.</em></p>
+            </div>
+            <div>
+                <input class="input-text" v-model="item.content" type="text" name="ListItem">
+                <div id="button" @click="add">Add</div>
+            </div>
+            <br>
+        </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'ListHeader',
+        data: function () {
+            return {
+                item: {},
+            }
+        },
+        methods: {
+            add: function () {
+                if (this.item.content) {
+                    this.$store.commit('addItem',{id:this.$store.state.itemList.length+1,content:this.item.content,status:false,editable:false});
+                    this.item = {};
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    /*@import "assets/css/style.css";*/
+
+</style>
