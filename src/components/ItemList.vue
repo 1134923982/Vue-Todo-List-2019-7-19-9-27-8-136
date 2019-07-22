2c7967cae0502d1f90d1dffb76f516ea4b1926a0
1134923982 <1134923982@qq.com>
2019-07-22 19:25:36 +0800

    [feat: create ItemList.vue component for todoList and use other component]

---
+++ src/components/ItemList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <div class="container">
+            <ListHeader></ListHeader>
+            <List/>
+            <ListButton/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ListHeader from "./ListHeader";
+    import List from "./List";
+    import ListButton from './ListButton'
+
+    export default {
+        name: 'ItemList',
+        components: {ListHeader,List,ListButton},
+    }
+</script>
+
